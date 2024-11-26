86d7013cb91df525112ba18f40cdd50f3477bbfe
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2024-11-26 11:10:26 +0000

    another changes

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,11 @@
 const express = require('express'); 
 const { getAllProducts, postProduct, updateProduct, deleteProduct } = require('../controllers/products');
 const adminMiddleware = require('../middlewares/adminMiddleware');
+const validateToken = require('../middlewares/tokenValidator');
 const router = express.Router(); 
 
 
-router.get('/', getAllProducts); 
+router.get('/',  validateToken ,  getAllProducts); 
 // use the admin middleware after this route
 router.post('/', adminMiddleware, postProduct);
 router.put('/:id', adminMiddleware, updateProduct);  
