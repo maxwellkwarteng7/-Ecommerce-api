15f24be7f64cf0a45f1fdd9b2e4e63f1fefdb1c5
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2025-02-02 23:55:57 +0000

    changes '

--- controllers/cart.js
+++ controllers/cart.js
@@ -3,14 +3,36 @@ const wrapper = require("express-async-handler");
 const { Cart, CartItems } = require("../models");
 const { BadRequestError } = require("../errors");
 
+
+
+async function AddProductToCart (cartItems , userId) {
+    //check if the user has a cart 
+    const hasCart = await Cart.findOne({ where: { userId } }); 
+    if (hasCart) {
+        for (let item of cartItems) {
+            const cartId = hasCart.id; 
+            const { productId, quantity } = item; 
+            
+            // check if the product already exist in the cartItem
+            const productExist = await CartItems.findOne({ where: { cartId, productId } }); 
+
+            if (productExist) {
+                productExist.quantity += quantity; 
+                
+            }
+        }
+    } else {
+        
+    }
+}
+
 const addToCart = wrapper(async (req, res) => {
-    const { productId } = req.body;
-    if (!productId) {
-        throw new BadRequestError('Product Id required'); 
+    const { cartItemsArray } = req.body;
+    if (!cartItemsArray || !Array.isArray(cartItemsArray)) {
+        throw new BadRequestError('cart Items are required or must be an array'); 
     }
     const { userId } = req;
-
-    // check if this user has a cart 
+    const cart = await AddProductToCart(cartItemsArray, userId);
 
    
     // check if user has a cart if not create one for user 
@@ -23,7 +45,7 @@ const addToCart = wrapper(async (req, res) => {
 
 
 module.exports = {
-    
+    addToCart
 }
 
 
