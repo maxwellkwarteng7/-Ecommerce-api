c19e65fcb7dad9cfa6f91f4fd043bf932d72bb3b
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2025-02-02 22:46:24 +0000

    changes

--- controllers/cart.js
+++ controllers/cart.js
@@ -2,7 +2,23 @@ const { StatusCodes } = require("http-status-codes");
 const wrapper = require("express-async-handler");
 const { Cart, CartItems } = require("../models");
 
+const addToCart = wrapper(async (req, res) => {
+    const { productId } = req.body;
+    const { userId } = req.userId;
+    console.log(req.userId, userId);
+    return;
+    // check if user has a cart if not create one for user 
+    const userCart = await Cart.findOne({ where: { userId } });
+}); 
 
+
+
+
+
+
+module.exports = {
+    addToCart
+}
 
 
 
